2c72a3b9e40b947e40ed284ed9fdb030e88cda01
mt0982 <tulikowski.maciej@gmail.com>
2017-06-03 11:06:22 +0200

    v2, clear data

--- gausskernel.cpp
+++ gausskernel.cpp
@@ -13,13 +13,10 @@ GaussKernel::GaussKernel(QWidget *parent): QWidget(parent), ui(new Ui::GaussKern
     ui->tableGauss->setColumnCount(3);
 
     for (int i = 0; i < 3; ++i) {
-        ui->tableGauss->setRowHeight(i, 20);
-        ui->tableGauss->setColumnWidth(i, 20);
+        ui->tableGauss->setRowHeight(i, 40);
+        ui->tableGauss->setColumnWidth(i, 40);
     }
 
-    //QTableWidgetItem *item = ui->tableGauss->item(0,0);
-    //item->setText("C");
-    //ui->tableGauss->setItem(0,0,item);
 }
 
 GaussKernel::~GaussKernel()
@@ -29,25 +26,40 @@ GaussKernel::~GaussKernel()
 
 void GaussKernel::on_sbRadius_valueChanged(int arg1)
 {
+    /* Clear */
+    ui->leSigma->clear();
+    ui->tableGauss->clear();
+
     arg1 = arg1 * 2 + 1;
 
     ui->tableGauss->setRowCount(arg1);
     ui->tableGauss->setColumnCount(arg1);
 
     for (int i = 0; i < arg1; ++i) {
-        ui->tableGauss->setRowHeight(i, 20);
-        ui->tableGauss->setColumnWidth(i, 20);
+        ui->tableGauss->setRowHeight(i, 40);
+        ui->tableGauss->setColumnWidth(i, 40);
     }
 }
 
 void GaussKernel::on_leSigma_textChanged(const QString &arg1)
 {
+    /* Clear */
+    ui->tableGauss->clear();
+
+    /* Parameters */
     int radius = ui->sbRadius->text().toInt();
     double sigma = arg1.toInt();
 
+    /* Init cell */
+    QTableWidgetItem *item;
+
     for (int x = 0; x < ui->tableGauss->rowCount(); ++x) {
         for (int y = 0; y < ui->tableGauss->colorCount(); ++y) {
+
             float value = exp(-(pow(x - radius,2) + pow(y - radius,2)) / (2 * pow(sigma,2)));
+            item = new QTableWidgetItem;
+            ui->tableGauss->setItem(x,y,item);
+            item->setText(QString::number(value));
         }
     }
 }

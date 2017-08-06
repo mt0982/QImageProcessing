9888e015ec701d7b703365fb5538b7913086405d
mt0982 <tulikowski.maciej@gmail.com>
2017-08-06 16:41:50 +0200

    backup

--- houghtransform.cpp
+++ houghtransform.cpp
@@ -18,21 +18,14 @@ HoughTransform::~HoughTransform()
 
 void HoughTransform::on_pbCalculate_clicked()
 {
-    /* Detect Edges */
+    /* Detect Edges - Canny */
+    QImage iOutput(image);
     canny->setImage(image);
     canny->processImage(1, 55, 100);
-//    sendImage(image);
 
-//    /* Accumulator */
-//    int rows = floor(sqrt(pow(image.width(), 2) + pow(image.height(), 2)));     //rows = max(diagonal)
-//    accumulator = new float*[rows];
-//    for (int i = 0; i < rows; ++i) {
-//        accumulator[i] = new float[180];                                        //180 = angle possibilities
-//    }
-
-
-    int height = floor(sqrt(pow(image.width(), 2) + pow(image.height(), 2)));   //rows = max(diagonal)
-    QImage iAccumulator(180, height, QImage::Format_Grayscale8);
+    /* Accumulato */
+    int height = floor(sqrt(pow(image.width(), 2) + pow(image.height(), 2)));       //rows = max(diagonal)
+    QImage iAccumulator(180, 2 * height, QImage::Format_Grayscale8);                //-R < height < R, R = diagonal
     iAccumulator.fill(0);
 
     for (int y = 0; y < image.height(); ++y) {
@@ -42,19 +35,34 @@ void HoughTransform::on_pbCalculate_clicked()
         for (int x = 0; x < image.width(); ++x) {
 
             if (ptr_image[x] != 0) {
-                for (int m = 1; m <= 180; m++) {
-                    int r = (x * cos((m * M_PI) / 180) + y * sin((m * M_PI) / 180));
-                    if (r > 0) {
-                        quint8 *ptr_iaccumulator = iAccumulator.scanLine(r);
-                        ptr_iaccumulator[m]++;
+                for (int m = 0; m < 180 * 3; m++) {
+                    int r = (int)(x * cos((m * M_PI) / 180) + y * sin((m * M_PI) / 180));
+                    if (r + height > 0) {
+                        quint8 *ptr_iaccumulator = iAccumulator.scanLine(r + height);
+                        ptr_iaccumulator[m] += (ptr_iaccumulator[m] + 1 < 255) ? 1 : 0;
                     }
                 }
             }
         }
     }
 
-    //iAccumulator = iAccumulator.scaled(image.width(), image.height(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+    iAccumulator = iAccumulator.scaled(image.width(), image.height(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
     sendImage(iAccumulator);
+
+//    /* */
+//    for (int y = 0; y < image.height(); ++y) {
+
+//        quint8 *ptr_iaccumulator = iAccumulator.scanLine(y);
+//        quint8 *ptr_ioutput = iOutput.scanLine(y);
+
+//        for (int x = 0; x < image.width(); ++x) {
+
+//            if (ptr_iaccumulator[x] > 240)
+//                ptr_ioutput[x] = qRgb(255,255,255);
+//        }
+//    }
+
+//    sendImage(iOutput);
 
 //    for (int i = 0; i < rows; ++i) {
 //        for (int j = 0; j < 180; ++j) {

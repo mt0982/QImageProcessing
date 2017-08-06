0cd3b3c0c1733fbb4b4c4042d819cb41342d88de
mt0982 <tulikowski.maciej@gmail.com>
2017-08-06 15:41:43 +0200

    hough space implemented

--- houghtransform.cpp
+++ houghtransform.cpp
@@ -21,14 +21,40 @@ void HoughTransform::on_pbCalculate_clicked()
     /* Detect Edges */
     canny->setImage(image);
     canny->processImage(1, 55, 100);
-    sendImage(image);
+//    sendImage(image);
 
-    /* Accumulator */
-    int rows = floor(sqrt(pow(image.width(), 2) + pow(image.height(), 2)));     //rows = max(diagonal)
-    accumulator = new float*[rows];
-    for (int i = 0; i < rows; ++i) {
-        accumulator[i] = new float[180];                                        //180 = angle possibilities
+//    /* Accumulator */
+//    int rows = floor(sqrt(pow(image.width(), 2) + pow(image.height(), 2)));     //rows = max(diagonal)
+//    accumulator = new float*[rows];
+//    for (int i = 0; i < rows; ++i) {
+//        accumulator[i] = new float[180];                                        //180 = angle possibilities
+//    }
+
+
+    int height = floor(sqrt(pow(image.width(), 2) + pow(image.height(), 2)));   //rows = max(diagonal)
+    QImage iAccumulator(180, height, QImage::Format_Grayscale8);
+    iAccumulator.fill(0);
+
+    for (int y = 0; y < image.height(); ++y) {
+
+        quint8 *ptr_image = image.scanLine(y);
+
+        for (int x = 0; x < image.width(); ++x) {
+
+            if (ptr_image[x] != 0) {
+                for (int m = 1; m <= 180; m++) {
+                    int r = (x * cos((m * M_PI) / 180) + y * sin((m * M_PI) / 180));
+                    if (r > 0) {
+                        quint8 *ptr_iaccumulator = iAccumulator.scanLine(r);
+                        ptr_iaccumulator[m]++;
+                    }
+                }
+            }
+        }
     }
+
+    //iAccumulator = iAccumulator.scaled(image.width(), image.height(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+    sendImage(iAccumulator);
 
 //    for (int i = 0; i < rows; ++i) {
 //        for (int j = 0; j < 180; ++j) {

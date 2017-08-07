0ca44fad53fbb25a9df049da3c30eb307166e9b5
mt0982 <tulikowski.maciej@gmail.com>
2017-08-07 22:10:21 +0200

    everything seems fine

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -16,7 +16,7 @@ MainWindow::MainWindow(QWidget *parent): QMainWindow(parent), ui(new Ui::MainWin
     connect(morfologicalFilter, SIGNAL(sendImage(QImage)), this, SLOT(setImage(QImage)));
     connect(houghTransform, SIGNAL(sendImage(QImage)), this, SLOT(setImage(QImage)));
 
-    QImage image("/home/asus/Obrazy/lenaNoise.png");
+    QImage image("/home/asus/Obrazy/fence.png");
     ui->labelImage->setPixmap(QPixmap::fromImage(image));
 
     gaussUnsharpFilter->setImage(image);
